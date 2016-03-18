2aa3b2d600026af8e3e2aa85898190b8b401a0a4
ArchKudo <vgd.vyas17@gmail.com>
2016-03-18 12:51:13 +0530

    Small Fixes

--- tutorial_01/input.c
+++ tutorial_01/input.c
@@ -8,20 +8,24 @@ void get_input(void) {
   // More at: https://goo.gl/jpdKDL
   SDL_Event event;
 
-  //SDL_PollEvent: Use this function to poll for currently pending events.
-  //More at: https://goo.gl/CHZGSq
+  // SDL_PollEvent: Use this function to poll for currently pending events.
+  // More at: https://goo.gl/CHZGSq
   while (SDL_PollEvent(&event)) {
-  	switch (event.type) {
-  		case SDL_QUIT:
-  			exit(0);
-  			break;
+    switch (event.type) {
+    case SDL_QUIT:
+      exit(0);
+      break;
 
-  		case SDL_KEYDOWN:
-  			switch(event.key.keysym.sym) {
-  				case SDLK_ESCAPE:
-  					exit(0);
-  					break;
-  			}
-  	}
+    case SDL_KEYDOWN:
+      switch (event.key.keysym.sym) {
+      case SDLK_ESCAPE:
+        exit(0);
+        break;
+      }
+      break;
+
+    default:
+      break;
+    }
   }
 }
