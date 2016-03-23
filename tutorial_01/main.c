91bc46488d89eb6d9ecae6c20f3c4bd245d9709a
ArchKudo <vgd.vyas17@gmail.com>
2016-03-23 19:57:41 +0530

    Better Logging, Add some #defines

--- tutorial_01/main.c
+++ tutorial_01/main.c
@@ -1,3 +1,4 @@
+// Opening a window
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,29 +6,29 @@
 #include "main.h"
 
 int main(int argc, char const *argv[]) {
-  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    fprintf(stderr, "Cannot Initialize SDL: %s\n", SDL_GetError());
-    exit(1);
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Cannot initialize SDL: %s",
+                 SDL_GetError());
+    return 3;
   }
 
   // man 3 atexit
   atexit(SDL_Quit);
-
+  SDL_Window *window =
+      SDL_CreateWindow("Displaying a window", POS, POS, WIDTH, HEIGHT, 0);
+  if (window == NULL) {
+    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Could not create a window: %s",
+                 SDL_GetError());
+    return 3;
+  }
   // SDL_Window creates a window with the specified position, dimensions, and
   // flags
   // Replaces SDL_SetVideoMode( https://goo.gl/lIoT1X )
   // More at: https://goo.gl/5KZkHn
-  SDL_Window *window_1 =
-      SDL_CreateWindow("Main Window", SDL_WINDOWPOS_UNDEFINED,
-                       SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_OPENGL);
-  if (window_1 == NULL) {
-    fprintf(stderr, "Couldn't create window: %s\n", SDL_GetError());
-    exit(1);
-  }
 
   while (1) {
-  	get_input();
-  	SDL_Delay(15);
+    get_input();
+    SDL_Delay(15);
   }
   return 0;
 }
--- tutorial_01/main.h
+++ tutorial_01/main.h
@@ -1,3 +1,7 @@
 #pragma once
 
+#define HEIGHT 480
+#define WIDTH 640
+#define POS SDL_WINDOWPOS_UNDEFINED
+
 void get_input(void);
